36f879400423317a9dd1b4a22ab480f505aa9061
victor brambilla <victor-brambilla@hotmail.com>
2023-09-19 11:28:20 -0400

    chore: add typeorm and pg

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,14 @@ import { GraphQLModule } from '@nestjs/graphql';
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '',
+      database: 'postgres',
+      entities: ['dist/**/*.model.js'],
+      synchronize: false,
     }),
   ],
 })
